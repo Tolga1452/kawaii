150064ce45973ddb36c229d3e8b2892ee49e6b8d
Tolga1452 <tolgchu-studios@outlook.com>
2024-05-02 20:49:00 +0300

    fix the url

--- src/url.ts
+++ src/url.ts
@@ -3,5 +3,5 @@ import { File, Path, Url } from './types.js';
 export function url(logo: Path): Url {
     const [part1, part2] = logo.split('/');
 
-    return `https://github.com/SAWARATSUKI/ServiceLogos/blob/main/${encodeURIComponent(part1)}/${encodeURIComponent(part2) as File}?raw=true`;
+    return `https://github.com/SAWARATSUKI/Logos/blob/main/${encodeURIComponent(part1)}/${encodeURIComponent(part2) as File}?raw=true`;
 };
