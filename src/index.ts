6917c5f9ba189f7cac84cd78e667724dc0994ace
Tolga1452 <tolgchu-studios@outlook.com>
2024-05-09 17:48:02 +0300

    fix logos

--- src/index.ts
+++ src/index.ts
@@ -18,67 +18,22 @@ app.get('/:logo', async (req, res) => {
 
     switch (name) {
         case '403': {
-            res.redirect(url('403Forbidden/Forbidden.png'));
+            res.redirect(url('ResponseCode/403 Forbidden.png'));
 
             break;
         };
         case '404': {
-            res.redirect(url('404Notfound/NotFound.png'));
+            res.redirect(url('ResponseCode/404 NotFound.png'));
 
             break;
         };
         case '418': {
-            res.redirect(url("418I'mateapot/418I'mateapot.png"));
+            res.redirect(url("ResponseCode/418 I'm a teapot.png"));
 
             break;
         };
         case '503': {
-            res.redirect(url('503ServiceUnavailable.png/ServiceUnavailable.png'));
-
-            break;
-        };
-        case 'angular': {
-            res.redirect(url('Angular/Angular.png'));
-
-            break;
-        };
-        case 'angular_old': {
-            res.redirect(url('Angular/Angular_Old.png'));
-
-            break;
-        };
-        case 'arch_linux': {
-            res.redirect(url('ArchLinux/ArchLinux.png'));
-
-            break;
-        };
-        case 'bluesky': {
-            res.redirect(url('Bluesky/Bluesky.png'));
-
-            break;
-        };
-        case 'cs_purple': {
-            res.redirect(url('C#/C# Purple.png'));
-
-            break;
-        };
-        case 'cs': {
-            res.redirect(url('C#/C#.png'));
-
-            break;
-        };
-        case 'cpp': {
-            res.redirect(url('C++/C++.png'));
-
-            break;
-        };
-        case 'c_all': {
-            res.redirect(url('C,C#,C++/All.png'));
-
-            break;
-        };
-        case 'c': {
-            res.redirect(url('C/C.png'));
+            res.redirect(url('ResponseCode/503 ServiceUnavailable.png'));
 
             break;
         };
